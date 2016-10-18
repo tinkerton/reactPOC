bc56941a34678015f00e15d61e727a4d837d5779
SnoWhat23 <brantare@gmail.com>
2016-10-18 10:27:42 +0200

    Reboot v2

--- js/reducers/list.js
+++ js/reducers/list.js
@@ -1,25 +1,25 @@
 
 import type { Action } from '../actions/types';
-import { SET_INDEX, SET_LIST, UPDATE_LIST_ITEM} from '../actions/list';
+import { SET_INDEX, SET_LIST, UPDATE_LIST_ITEM } from '../actions/list';
 
 export type State = {
-    list: object,
+  list: object,
 }
 
 const initialState = {
   list: [
-    {name:'jsonItem1', id:'5550001'},
-    {name:'jsonItem2', id:'5550002'},
-    {name:'jsonItem3', id:'5550003'},
-    {name:'jsonItem4', id:'5550004'},
-    {name:'jsonItem5', id:'5550005'},
-    {name:'jsonItem6', id:'5550006'},
-    {name:'jsonItem7', id:'5550007'}
+    { name: 'jsonItem1', id: '5550001' },
+    { name: 'jsonItem2', id: '5550002' },
+    { name: 'jsonItem3', id: '5550003' },
+    { name: 'jsonItem4', id: '5550004' },
+    { name: 'jsonItem5', id: '5550005' },
+    { name: 'jsonItem6', id: '5550006' },
+    { name: 'jsonItem7', id: '5550007' }
   ],
   selectedIndex: undefined,
 };
 
-export default function (state:State = initialState, action:Action): State {
+export default function (state: State = initialState, action: Action): State {
   if (action.type === SET_INDEX) {
     return {
       ...state,
@@ -33,9 +33,12 @@ export default function (state:State = initialState, action:Action): State {
     };
   }
   if (action.type === UPDATE_LIST_ITEM) {
-      return {
+    return {
       ...state,
-      list: state.list.slice(0, state.selectedIndex).concat(action.payload).concat(state.list.slice(state.selectedIndex+1, state.list.length)),
+      list: state.list
+      .slice(0, state.selectedIndex)
+      .concat(action.payload)
+      .concat(state.list.slice(state.selectedIndex + 1, state.list.length)),
     };
   }
 
